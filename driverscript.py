04903f6c962a61060002cbc46b510e48de1eb459
tantrumkiller <joshikshitij_13@yahoo.in>
2014-08-04 20:41:53 +0530

    Update driverscript.py

--- driverscript.py
+++ driverscript.py
@@ -6,6 +6,6 @@ GroupId = ""
 # this graph api may change
 request_url = "https://graph.facebook.com/"+GroupId+"/feed?&access_token="+AccessToken
 
-GenrateAnalytics.installProxy('','','202.141.80.19','3128')
+GenrateAnalytics.installProxy('','','','')
 users = GenrateAnalytics.populateData(request_url,"time_logs",AccessToken)
 GenrateAnalytics.dumpData("",users)
